fe4a38b3a1d6886e3dfa66f014da5ab889511c4b
Ihor Stefaniv <dezir16@gmail.com>
2020-06-01 21:18:52 +0300

    add code on ihor.js

--- js/ihor.js
+++ js/ihor.js
@@ -1,8 +1,15 @@
-const $newCar = document.getElementById('choose-new-car');
+const $newCar = document.getElementById('.choose_new_car');
+const $brandCar = document.getElementById('.brand_car');
+const $yearCar = document.getElementById('year_car');
+const $colorCar = document.getElementById('color_car');
 
+console.log()
 console.log($newCar);
 
 $newCar.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log('New car');
+  console.log('$brandCar.value');
+  console.log('$yearCar.value');
+  console.log('$colorCar.value');
 });
